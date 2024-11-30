aa3797037d51c66b80e259b07f3003a3adbba3c9
d.bystrov <>
2024-11-30 17:43:33 +0400

    change model task

--- src/main/java/org/taskmanager/model/Task.java
+++ src/main/java/org/taskmanager/model/Task.java
@@ -12,7 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Task {
-    private long id;
+    private Long id;
     private String text;
     private boolean completed;
 }
